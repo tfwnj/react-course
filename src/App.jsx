96042e5217333396d827fd2a336ee88fc276d652
tfwnj <pqlv2026@gmail.com>
2026-02-19 06:05:54 -0300

    ecommerce website

--- src/App.jsx
+++ src/App.jsx
@@ -1,117 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
 
-import { use, useContext, useState } from 'react';
-import './App.css';
-import SignupForm from './components/SignUpForm';
-import { Routes, Route, Link } from 'react-router-dom';
-import { AuthContext } from './AuthContext';
-
-function Navbar(){
-  const {user, logout} = useContext(AuthContext);
-    return(
-        <header
-            style={{
-                padding: "1rem 1.5rem",
-                marginBottom: "1rem",
-                borderBottom: "1px solid #e5e7eb",
-                display: "flex",
-                justifyContent: "space-between",
-            }}
-        >
-            <nav style={{display: "flex", gap: "1rem"}}>
-                <Link to="/">Home</Link>
-                <Link to="/profile">Profile</Link>
-            </nav>
-
-            <div>
-                {!user.isAuth ? <Link to="/login">Login</Link> : <button onClick={logout}>Logout</button> }
-            </div>
-        </header>
-    )
-}
-
-function HomePage(){
-  const {user} = useContext(AuthContext);
+function App(){
   return (
-    <div style={{padding: "0 1.5rem"}}>
-      <h1>Home</h1>
-
-      {user.isAuth ? (
-        <p>Welcome back, {user.name}! %</p>
-      ) : (
-        <p>You are not logged in. Go to the login page to sign in.</p>
-      )}
-    </div>
-  );
-}
-
-function LoginPage(){
-  const [name, setName] = useState("");
-  const {user, login} = useContext(AuthContext);
-
-  function handleSubmit(e){
-    e.preventDefault();
-    if(!name.trim()) return;
-    login(name);
-  }
-
-  return (
-    <div style={{padding: "0 1.5rem"}}>
-      <h1>Login</h1>
-
-      <form onSubmit={handleSubmit} style={{marginTop: "1rem"}}>
-        <label htmlFor="">
-          Name
-          <input type="text" placeholder="Type any name..." value={name} 
-          onChange={(e) => setName(e.target.value)} style={{marginLeft: "0.5rem"}} />
-        </label>
-        <button type='submit' style={{marginLeft: "0.5rem"}}>Log in</button>
-      </form>
-      {user.isAuth && <p>User Logged In</p>}
-    </div>
-  );
-}
-
-function ProfilePage(){
-  const {user} = useContext(AuthContext);
-  return (
-    <div style={{padding: "0 1.5rem"}}>
-      <h1>Profile</h1>
-      <p>Name: {user.name}</p>
-      <p>Here you could show more user info from the context</p>
-    </div>
-  );
-}
-
-function App() {
-  const [user, setUser] = useState({name: "", isAuth: false});
-
-  function login(name){
-    setUser({name: name, isAuth: true});
-  }
-
-  function logout(){
-    setUser({name: "", isAuth: false});
-  }
-
-  return (
-    <div>
-      <AuthContext.Provider value={{user, login, logout}}>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="*"
-            element={
-              <h1 style={{padding: "0 1.5rem"}}>
-                404 Not Found
-              </h1>
-            } 
-          />
-        </Routes>
-      </AuthContext.Provider>
-      <footer>Footer</footer>
+    <div className="app">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/auth" element={<Auth/>} />
+        <Route path="/checkout" element={<Checkout/>} />
+      </Routes>
     </div>
   );
 }
